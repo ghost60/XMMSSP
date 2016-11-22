07a19e5c926184c967c42319aa3b329ef4656489
ghost60 <570761003@qq.com>
2016-11-22 15:57:15 +0800

    lsq

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,7 +11,6 @@ class Nav extends React.Component{
         debugger
         const list = this.props.navlist.map( (list,i) => {
             if (list.id==0) {
-                console.log('路由跳转到 /');
                 return  <div className="nav_li" key={list.id}>
                         <span>
                             <IndexLink to = "/" activeClassName="active">
@@ -20,7 +19,6 @@ class Nav extends React.Component{
                         </span>
                         </div>
             }else{
-                console.log('路由跳转到:'+list.type);
                 return  <div className="nav_li" key={list.id}>
                         <span>
                             <Link to={list.type} activeClassName="active">
