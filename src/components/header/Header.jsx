0ebe2277d8cfbd864516422ab9dc420b90bff718
GoDotDotDot <chukui0202@gmail.com>
2016-11-22 15:50:50 +0800

    add

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css'
+export default class Header extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+    render(){
+        return(
+            <div>
+                <div className="header-bg1"></div>
+                <div className="header-bg2">
+                    <div>
+
+                    </div>
+                    <div>
+                    
+                    </div>                    
+                </div>
+            </div>
+        )
+    }
+}
