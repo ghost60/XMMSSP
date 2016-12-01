cf5383e5d1596a70c55201b9ddf73d59ab4284d7
GoDotDotDot <chukui0202@gmail.com>
2016-12-01 19:10:40 +0800

    edit by chukui

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -4,44 +4,55 @@ import { Link } from 'react-router'
 import * as menudata from '../../pages/menudata/menudata';
 import './Aside.scss';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-
+var aside_content;
 class aside extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { name: '', ul_item: [] };
+        this.state = { name: '', currentIndex: 0 };
+    }
+    asideParentTitleClick(index) {
+        this.setState({ currentIndex: index });
+    }
+    getAsideChildUlCls(index) {
+        return this.state.currentIndex === index ? "aside-ul aside-child-ul aside-child-ul__active" : "aside-ul aside-child-ul";
     }
     updateaside(tprops) {
-        // debugger
+
+        // debugger aside-child-ul__active
         var list_item = [];
         var ul_item = [];
         var product = menudata.navlist[tprops.parent];
         var list = product.aside || product.children[tprops.link].aside;//子项目
-        var name = product.name;//一级中文名与路由同名
+
         var list_child_item = [];
         var child_link = "";
         for (var i = 0; i < list.length; ++i) {
             //侧边栏二级子路由
             if (list[i].aside && list[i].aside.length > 0) {
                 for (var j = 0; j < list[i].aside.length; ++j) {
-                    child_link = tprops.parent + "/" + (product.aside ? tprops.parent + "Session/" : tprops.link + "/" + tprops.link + "Session/")+ list[i].aside[j].name;
+                    child_link = tprops.parent + "/" + (product.aside ? tprops.parent + "Session/" : tprops.link + "/" + tprops.link + "Session/") + list[i].aside[j].name;
                     list_child_item.push(<li className="aside-child-li" key={"child_" + i + "_" + j}><Link to={child_link} activeClassName="active">{list[i].aside[j].name}</Link></li>);
                 }
-                list_item.push(<li key={i} className="aside-parent-li"><div className="aside-parent-title"><Link>{list[i].name}</Link></div><ReactCSSTransitionGroup key={"animate_" + i} component="div" transitionName="asideAnimate" transitionAppear={true} transitionAppearTimeout={300000} transitionEnterTimeout={300} transitionLeaveTimeout={300}><ul className="aside-ul aside-child-ul">{list_child_item}</ul></ReactCSSTransitionGroup></li>);
-                list_child_item =[];
+                list_item.push(<li key={i} className="aside-parent-li"><div className="aside-parent-title" onClick={this.asideParentTitleClick.bind(this, i)}><Link>{list[i].name}</Link></div><ul className={this.getAsideChildUlCls(i)}>{list_child_item}</ul></li>);
+                list_child_item = [];
             }
             else {
                 //侧边栏一级路由
-                child_link = tprops.parent + "/" + (product.aside ? tprops.parent + "Session/" : tprops.link + "/" + tprops.link + "Session/")+ list[i].name;
+                child_link = tprops.parent + "/" + (product.aside ? tprops.parent + "Session/" : tprops.link + "/" + tprops.link + "Session/") + list[i].name;
                 list_item.push(<li key={i} className="aside-parent-li"><div className="aside-parent-title"><Link to={child_link}>{list[i].name}</Link></div></li>)
             }
         }
         ul_item.push(<ul className="aside-ul aside-parent-ul" key={'aside_ul'}>{list_item}</ul>)
         list_item = null;
-        this.setState({ name: name, ul_item: ul_item });
-        ul_item = null;
+        return  ul_item 
     }
-    componentWillMount() {
-        this.updateaside(this.props);
+    componentDidMount() {
+        // this.updateaside(this.props);
+        var name = menudata.navlist[this.props.parent].name;//一级中文名与路由同名
+        this.setState({ name: name });
+    }
+    getContent() {
+        return aside_content
     }
     componentWillReceiveProps(nextProps) {
         // this.updateaside(nextProps);
@@ -50,10 +61,11 @@ class aside extends React.Component {
         return <div>
             <div className="aside_title">{this.state.name}</div>
             <div className="aside_list">
-                {this.state.ul_item}
+                {this.updateaside(this.props)}
             </div>
         </div>
     }
 };
 
 export default aside;
+//<ReactCSSTransitionGroup key={"animate_" + i} component="div" transitionName="asideAnimate" transitionAppear={false} transitionAppearTimeout={300000} transitionEnterTimeout={300} transitionLeaveTimeout={300} style={{height:"100%"}}><ul className={this.getAsideChildUlCls(i)}>{list_child_item}</ul></ReactCSSTransitionGroup>
