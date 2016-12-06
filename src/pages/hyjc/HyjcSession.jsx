7a698d9a98a1bbda390b384de753dfadeca429a3
ghost60 <570761003@qq.com>
2016-12-06 10:05:52 +0800

    lsq

---
+++ src/pages/hyjc/HyjcSession.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Session from '../../components/session/Session';
+import * as menudata from '../../pages/menudata/menudata';
+import "./Hyjc.css"
+
+class hyjcsession extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '',src:''};
+  }
+  componentDidMount() {
+    this.addname(this.props);
+  }
+  addname(mprops){
+    debugger
+    if (!mprops.params) {
+      var menu = menudata.navlist.hyjc.children;
+      var key = Object.keys(menu)[0];
+      menu = menu[key];
+      this.setState({ name: menu.name, src: key });
+    }else{
+      menu = menudata.navlist.hyjc.children;
+      for (var key in menu) {
+        if (key == mprops.params.cid) {
+          this.setState({ name: menu[key].name, src: key });
+        }
+      }
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    this.addname(this.props);
+  }
+  setIframe() {
+    var iframeWin = this.refs.iframe.contentWindow || this.refs.iframe.contentDocument.parentWindow;
+    if (iframeWin.document.body) {
+      var height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+      this.refs.iframe.height = height+"px"
+    }
+  }
+  render() {  
+    var src=`data/HYJC/${this.state.src}.html`;
+    return  <Session lastname={this.state.name} name={"/海洋监测"}>
+              <iframe ref="iframe" src={src} className="iframe" onLoad={this.setIframe.bind(this)}>
+              </iframe>
+            </Session>
+  }
+};
+
+export default hyjcsession;
