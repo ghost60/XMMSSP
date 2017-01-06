e9c4ef7355ec640f2ccd89bb9ad7d581efc62ea9
ghost60 <570761003@qq.com>
2017-01-06 17:15:29 +0800

    lsq

---
+++ dist/js/ie.js
@@ -0,0 +1,4 @@
+(function() {
+	if (navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE6.0" || navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE7.0" || navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0" || navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE9.0")
+		window.location.href = "tishi.html";
+}());
